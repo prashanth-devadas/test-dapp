979d6976a28c2f73aefa400b6ed3d61bb4b51abb
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-07-21 01:36:40 -0400

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+const { task } = require("hardhat/config");
+
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("balances", "Gets the default address balances", async(taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  const provider = await hre.ethers.provider;
+
+  for (const account of account) {
+    console.log(provider.getBalance(account.address))
+  }
+  
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+
+  networks:{
+    hardhat: {
+      chainId: 1337,
+    }
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import logo from './logo.svg';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
 
 function App() {
+  const [greeting, setGreeting] = useState();
+
+  async function requestAccount(){
+    await window.ethereum.requestAccount({ method: 'eth_requestAccounts'});
+  }
+
+  async function fetchGreeting(){
+    if(typeof window.ethereum !== 'undefined'){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      
+      try{
+        const data = await contract.greet();
+        console.log("data: ", data);
+      } catch (err){
+        console.log("error: ", err);
+      }
+    }
+  }
+
+  async function setGreetingValue(){
+    if(!greeting) return;
+    if(typeof window.ethereum !== 'undefined'){
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = new provider.getSigner();
+      const contract = new ethers.contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
